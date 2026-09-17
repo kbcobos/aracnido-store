15fcbcd32aed451d8812a4ba8c3a9024c7064238
Katherine Cobos <kcoboscaballero@gmail.com>
2026-09-16 23:13:45 -0300

    Estructura inicial de paquetes y menu base

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+        mostrarMenu();
+    }
+
+    private static void mostrarMenu() {
+        System.out.println("=================================== SISTEMA DE GESTIÓN - ARÁCNIDO STORE ==================================");
+        System.out.println();
+        System.out.println("1) Agregar producto");
+        System.out.println("2) Listar productos");
+        System.out.println("3) Buscar/Actualizar producto");
+        System.out.println("4) Eliminar producto");
+        System.out.println("5) Crear un pedido");
+        System.out.println("6) Listar pedidos");
+        System.out.println("7) Salir");
+        System.out.println();
+        System.out.print("Elija una opción: ");
+    }
+}
