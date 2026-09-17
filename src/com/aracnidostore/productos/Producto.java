a335aff6b60e6fd353b91e9b827d0e23a57e855a
Katherine Cobos <kcoboscaballero@gmail.com>
2026-09-17 00:19:47 -0300

    Agregando clase Producto

---
+++ src/com/aracnidostore/productos/Producto.java
@@ -0,0 +1,54 @@
+package com.aracnidostore.productos;
+
+public class Producto {
+
+    private static int contador = 1;
+
+    private final int id;
+    private String nombre;
+    private double precio;
+    private int stock;
+
+    public Producto(String nombre, double precio, int stock) {
+        this.id = contador++;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id +
+                " | Nombre: " + nombre +
+                " | Precio: $" + precio +
+                " | Stock: " + stock;
+    }
+}
